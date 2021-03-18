40279a32a67cb03e59ce3e2de197b2247ee5214d
Marc Riera Casals <mrc2407@gmail.com>
2021-03-18 19:27:02 +0100

    Convert JSON to YAML frontmatter and save in file

--- json_to_yaml.rb
+++ json_to_yaml.rb
@@ -5,28 +5,30 @@ require "json"
 Dir["data/*waylos*"].each do |json_file_name|
   lemma = Lemma.from_json_file(json_file_name)
 
-  puts "---"
-  puts "title: #{lemma.lemma}"
-  puts "klass: #{lemma.klass.downcase}"
-  puts "etymology:"
-  lemma.etymology.each do |element|
-    puts "- #{element}"
+  File.open("site/_pie_lemmas/#{lemma.lemma_to_order}.md", "w") do |f|
+    f.puts "---"
+    f.puts "title: \"#{lemma.lemma}\""
+    f.puts "klass: #{lemma.klass.downcase}"
+    f.puts "etymology:"
+    lemma.etymology.each do |element|
+      f.puts "- #{element}"
+    end
+    f.puts "markers:"
+    lemma.markers.each do |element|
+      f.puts "- #{element}"
+    end
+    f.puts "synonyms:"
+    lemma.synonyms.each do |element|
+      f.puts "- #{element}"
+    end
+    f.puts "definitions:"
+    lemma.definitions.each do |element|
+      f.puts "- #{element}"
+    end
+    f.puts "topics:"
+    lemma.topics.each do |element|
+      f.puts "- #{element}"
+    end
+    f.puts "---"
   end
-  puts "markers:"
-  lemma.markers.each do |element|
-    puts "- #{element}"
-  end
-  puts "synonyms:"
-  lemma.synonyms.each do |element|
-    puts "- #{element}"
-  end
-  puts "definitions:"
-  lemma.definitions.each do |element|
-    puts "- #{element}"
-  end
-  puts "topics:"
-  lemma.topics.each do |element|
-    puts "- #{element}"
-  end
-  puts "---"
 end
