c88ee992e114d4b98e7298a203a90a2696d73b3a
Marc Riera Casals <mrc2407@gmail.com>
2021-03-19 17:47:08 +0100

    Show definitions by initial

--- site/_plugins/group_by_initial_filter.rb
+++ site/_plugins/group_by_initial_filter.rb
@@ -1,6 +1,13 @@
 module GroupByInitialFilter
   def group_by_initial(array)
-    array.group_by { |elem| elem.data["lemma_to_sort"][0].upcase }
+    array.group_by do |elem|
+      initial = if elem.is_a?(String)
+                  elem[0]
+                else
+                  elem.data["lemma_to_sort"][0]
+                end
+      initial.upcase
+    end
   end
 end
 Liquid::Template.register_filter(GroupByInitialFilter)
